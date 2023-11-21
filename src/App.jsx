f294e4cf8889439a721dd6e2fa882c4b0d211a86
zamami <s1413115@gmail.com>
2023-11-21 19:38:07 +0900

    コンポーネントの間に挟むことでchildrenとしてpropsの値を渡すことができる

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ export const App = () => {
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColoredMessage color="blue" message="お元気ですか？" />
-      <ColoredMessage color="pink" message="元気です!" />
+      <ColoredMessage color="blue"> お元気ですか？ </ColoredMessage>
+      <ColoredMessage color="pink"> 元気です! </ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,8 +1,10 @@
+
+// propsを使うことでコンポーネントを動的に扱うことができるようになった。
 export const ColoredMessage = (props) => {
   const contentStyle = {
     color: props.color,
    fontSize: "20px"
   };
 
-  return <p style={ contentStyle }>{props.message}</p>;
+  return <p style={ contentStyle }>{props.children}</p>; // htmlの中では{}の中にjsを書く
 };
