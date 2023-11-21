9c8431de522fd9d621918a15c87776d31534f42a
zamami <s1413115@gmail.com>
2023-11-21 19:46:25 +0900

    分割代入と省略記法をpropsで使用する

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,10 +1,13 @@
 
 // propsを使うことでコンポーネントを動的に扱うことができるようになった。
 export const ColoredMessage = (props) => {
+
+  const { color, children } = props; //propsを分割代入
   const contentStyle = {
-    color: props.color,
+  //  color: color,   props.colorと書かなくて良くなった。
+   color, //さらにキーとバリューが同じ名前なので省略記法で書くことができる
    fontSize: "20px"
   };
 
-  return <p style={ contentStyle }>{props.children}</p>; // htmlの中では{}の中にjsを書く
+  return <p style={ contentStyle }>{children}</p>; // htmlの中では{}の中にjsを書く
 };
