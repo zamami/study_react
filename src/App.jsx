fe3cd36018dc459d39a64a0c6fb8dc26496b8f27
zamami <s1413115@gmail.com>
2023-11-21 17:44:30 +0900

    イベント実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 // コンポーネントのファイルは.jsx
 
 export const App = () => {
+
+  /* {}を使ってjsを書くこともできる */
+  const onClickButton = () => {
+    alert();
+  };
+
   return (
     <>
       <h1>こんにちは！</h1>
       <p>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
